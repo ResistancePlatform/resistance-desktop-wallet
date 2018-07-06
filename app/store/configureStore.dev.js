cd989a7ea2b245a85224418866936e9953c2f71d
WisonYe <wisonye@gmail.com>
2018-07-06 21:30:37 +0800

    [ concept-proof ] - Removed the unuseful 'redux-thunk', added latest 'redux-observable' for the async action support, fixed the 'store' for using 'redux-observable'.

--- app/store/configureStore.dev.js
+++ app/store/configureStore.dev.js
@@ -1,67 +1,56 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import { createHashHistory } from 'history';
-import { routerMiddleware, routerActions } from 'react-router-redux';
-import { createLogger } from 'redux-logger';
-import rootReducer from '../reducers';
-import * as counterActions from '../actions/counter';
-import type { counterStateType } from '../reducers/counter';
+import { createStore, applyMiddleware, combineReducers, Store } from 'redux'
+import { combineEpics, createEpicMiddleware } from 'redux-observable'
+import { createHashHistory } from 'history'
+import { routerMiddleware } from 'react-router-redux'
+import { createLogger } from 'redux-logger'
 
-const history = createHashHistory();
+import { OverviewReducer } from '../reducers/overview'
+import { OverviewEpics } from '../reducers/overview.epic'
 
-const configureStore = (initialState?: counterStateType) => {
+export const history = createHashHistory()
+
+const appReducers = combineReducers({
+  overview: OverviewReducer
+})
+
+export const appEpics = combineEpics(
+  OverviewEpics
+)
+
+const epicMiddleware = createEpicMiddleware()
+
+export let appStore: Store = null;
+
+export const configureStore = (initialState) => {
   // Redux Configuration
-  const middleware = [];
-  const enhancers = [];
-
-  // Thunk Middleware
-  middleware.push(thunk);
+  const middleware = []
 
   // Logging Middleware
   const logger = createLogger({
     level: 'info',
     collapsed: true
-  });
+  })
 
   // Skip redux logs in console during the tests
   if (process.env.NODE_ENV !== 'test') {
-    middleware.push(logger);
+    middleware.push(logger)
   }
 
   // Router Middleware
   const router = routerMiddleware(history);
   middleware.push(router);
 
-  // Redux DevTools Configuration
-  const actionCreators = {
-    ...counterActions,
-    ...routerActions
-  };
-  // If Redux DevTools Extension is installed use it, otherwise use Redux compose
-  /* eslint-disable no-underscore-dangle */
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Options: http://zalmoxisus.github.io/redux-devtools-extension/API/Arguments.html
-        actionCreators
-      })
-    : compose;
-  /* eslint-enable no-underscore-dangle */
-
-  // Apply Middleware & Compose Enhancers
-  enhancers.push(applyMiddleware(...middleware));
-  const enhancer = composeEnhancers(...enhancers);
+  // Epic middleware
+  middleware.push(epicMiddleware);
 
   // Create Store
-  const store = createStore(rootReducer, initialState, enhancer);
+  appStore = createStore(
+    appReducers,
+    initialState,
+    applyMiddleware(...middleware)
+  )
 
-  if (module.hot) {
-    module.hot.accept(
-      '../reducers',
-      () => store.replaceReducer(require('../reducers')) // eslint-disable-line global-require
-    );
-  }
+  epicMiddleware.run(appEpics)
 
-  return store;
-};
-
-export default { configureStore, history };
+  return appStore
+}
--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,17 +1,30 @@
 // @flow
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { createHashHistory } from 'history';
-import { routerMiddleware } from 'react-router-redux';
-import rootReducer from '../reducers';
-import type { counterStateType } from '../reducers/counter';
+import { createStore, combineReducers, applyMiddleware, Store } from 'redux'
+import { combineEpics, createEpicMiddleware } from 'redux-observable'
+import { createHashHistory } from 'history'
+import { routerMiddleware } from 'react-router-redux'
 
-const history = createHashHistory();
-const router = routerMiddleware(history);
-const enhancer = applyMiddleware(thunk, router);
+import { OverviewReducer } from '../reducers/overview'
+import { OverviewEpics } from '../reducers/overview.epic'
 
-function configureStore(initialState?: counterStateType) {
-  return createStore(rootReducer, initialState, enhancer);
+export const history = createHashHistory()
+const appReducers = combineReducers({
+  overview: OverviewReducer
+})
+
+export const appEpics = combineEpics(
+  OverviewEpics
+)
+
+const epicMiddleware = createEpicMiddleware()
+
+export let appStore: Store = null
+const router = routerMiddleware(history)
+const middleware = [router, epicMiddleware]
+const enhancer = applyMiddleware(...middleware)
+
+export const configureStore = (initialState) => {
+  appStore = createStore(appReducers, initialState, enhancer)
+  epicMiddleware.run(appEpics)
+  return appStore
 }
-
-export default { configureStore, history };
