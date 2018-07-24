9cb004d9b8a5059af66398f180a58d856d50ce55
Konstantin Alexandrov <negus@iwuvjhdva-2.local>
2018-07-24 13:21:57 +0200

    Start/stop local node button display implemented

--- app/containers/settings/settings.js
+++ app/containers/settings/settings.js
@@ -1,206 +1,215 @@
 // @flow
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import RounedInput, { RoundedInputAddon } from '../../components/rounded-input'
+import { SystemInfoState } from '../../state/reducers/system-info/system-info.reducer';
+import RounedInput, { RoundedInputAddon } from '../../components/rounded-input';
 // import { SendCashActions, SendCashState } from '../../state/reducers/send-cash/send-cash.reducer'
-// import { appStore } from '../../state/store/configureStore'
-import styles from './settings.scss'
-import HLayout from '../../theme/h-box-layout.scss'
-import VLayout from '../../theme/v-box-layout.scss'
-
+import styles from './settings.scss';
+import HLayout from '../../theme/h-box-layout.scss';
+import VLayout from '../../theme/v-box-layout.scss';
 
 type Props = {
-	// sendCash: SendCashState
-}
-
+  systemInfo: SystemInfoState
+  // sendCash: SendCashState
+};
 
 /**
  * @class Settings
  * @extends {Component<Props>}
  */
 class Settings extends Component<Props> {
-	props: Props
+  props: Props;
 
-	/**
-	 * @memberof Settings
-	 */
-	componentDidMount() {
-	}
+  /**
+   * @memberof Settings
+   */
+  componentDidMount() {}
 
+  /**
+   * @param {*} event
+   * @memberof Settings
+   */
+  eventConfirm(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
 
-	/**
-	 * @param {*} event
-	 * @memberof Settings
-	 */
-	eventConfirm(event) {
-		event.preventDefault()
-		event.stopPropagation()
-	}
+  /**
+   * @param {*} value
+   * @memberof Settings
+   */
+  onOldPasswordInputChanged(value) {
+    console.log(`onOldPasswordInputChanged: ${value}`);
+  }
 
-	/**
-	 * @param {*} value
-	 * @memberof Settings
-	 */
-	onOldPasswordInputChanged(value) {
-		console.log(`onOldPasswordInputChanged: ${value}`)
-	}
+  /**
+   * @param {*} value
+   * @memberof Settings
+   */
+  onNewPasswordInputChanged(value) {
+    console.log(`onNewPasswordInputChanged: ${value}`);
+  }
 
-	/**
-	 * @param {*} value
-	 * @memberof Settings
-	 */
-	onNewPasswordInputChanged(value) {
-		console.log(`onNewPasswordInputChanged: ${value}`)
-	}
+  /**
+   * @param {*} value
+   * @memberof Settings
+   */
+  onRepeatPasswordInputChanged(value) {
+    console.log(`onRepeatPasswordInputChanged: ${value}`);
+  }
 
-	/**
-	 * @param {*} value
-	 * @memberof Settings
-	 */
-	onRepeatPasswordInputChanged(value) {
-		console.log(`onRepeatPasswordInputChanged: ${value}`)
-	}
+  /**
+   * @param {*} event
+   * @memberof Settings
+   */
+  onSavePasswordClicked(event) {
+    this.eventConfirm(event);
+    console.log(`onSavePasswordClicked---->`);
+  }
 
-	/**
-	 * @param {*} event
-	 * @memberof Settings
-	 */
-	onSavePasswordClicked(event) {
-		this.eventConfirm(event);
-		console.log(`onSavePasswordClicked---->`)
-	}
+  /**
+   * @param {*} event
+   * @memberof Settings
+   */
+  onStopLocalNodeClicked(event) {
+    this.eventConfirm(event);
+    // appStore.dispatch(SettingsActions.stopStartLocalNode())
+  }
 
-	/**
-	 * @param {*} event
-	 * @memberof Settings
-	 */
-	onStopLocalNodeClicked(event) {
-		this.eventConfirm(event);
-		console.log(`onStopLocalNodeClicked---->`)
-	}
+  /**
+   * @param {*} event
+   * @memberof Settings
+   */
+  onShowStatusClicked(event) {
+    this.eventConfirm(event);
+    console.log(`onShowStatusClicked---->`);
+  }
 
-	/**
-	 * @param {*} event
-	 * @memberof Settings
-	 */
-	onShowStatusClicked(event) {
-		this.eventConfirm(event);
-		console.log(`onShowStatusClicked---->`)
-	}
+  /**
+   * @param {*} event
+   * @memberof Settings
+   */
+  onBackupWalletClicked(event) {
+    this.eventConfirm(event);
+    console.log(`onBackupWalletClicked---->`);
+  }
 
-	/**
-	 * @param {*} event
-	 * @memberof Settings
-	 */
-	onBackupWalletClicked(event) {
-		this.eventConfirm(event);
-		console.log(`onBackupWalletClicked---->`)
-	}
+  /**
+   * @returns
+   * @memberof Settings
+   */
+  render() {
+    const passwordAddon: RoundedInputAddon = {
+      enable: false,
+      type: 'TEXT_PLACEHOLDER',
+      onAddonClicked: null,
+      value: ''
+    };
 
+    return (
+      // Layout container
+      <div
+        className={[
+          styles.layoutContainer,
+          HLayout.hBoxChild,
+          VLayout.vBoxContainer
+        ].join(' ')}
+      >
+        {/* Route content */}
+        <div
+          className={[
+            styles.settingsContainer,
+            VLayout.vBoxChild,
+            HLayout.hBoxContainer
+          ].join(' ')}
+        >
+          <div
+            className={[
+              HLayout.hBoxChild,
+              VLayout.vBoxContainer,
+              styles.wrapperContainer
+            ].join(' ')}
+          >
+            {/* Title bar */}
+            <div className={styles.titleBar}>Settings</div>
 
-	/**
-	 * @returns
-	 * @memberof Settings
-	 */
-	render() {
-		const passwordAddon: RoundedInputAddon = {
-			enable: false,
-			type: 'TEXT_PLACEHOLDER',
-			onAddonClicked: null,
-			value: ''
-		}
+            {/* Old password */}
+            <RounedInput
+              name="old-password"
+              title="OLD PASSWORD"
+              addon={passwordAddon}
+              onInputChange={value => this.onOldPasswordInputChanged(value)}
+            />
 
-		return (
-			// Layout container
-			<div className={[styles.layoutContainer, HLayout.hBoxChild, VLayout.vBoxContainer].join(' ')}>
+            {/* New password */}
+            <RounedInput
+              name="new-password"
+              title="NEW PASSWORD"
+              addon={passwordAddon}
+              onInputChange={value => this.onNewPasswordInputChanged(value)}
+            />
 
-				{ /* Route content */}
-				<div className={[styles.settingsContainer, VLayout.vBoxChild, HLayout.hBoxContainer].join(' ')}>
+            {/* Repeat password */}
+            <RounedInput
+              name="repeat-password"
+              title="REPEAT NEW PASSWORD"
+              addon={passwordAddon}
+              onInputChange={value => this.onRepeatPasswordInputChanged(value)}
+            />
 
-					<div className={[HLayout.hBoxChild, VLayout.vBoxContainer, styles.wrapperContainer].join(' ')}>
+            {/* Save password */}
+            <button
+              className={styles.savePasswordButton}
+              onClick={event => this.onSavePasswordClicked(event)}
+              onKeyDown={event => this.onSavePasswordClicked(event)}
+            >
+              SAVE PASSWORD
+            </button>
 
-						{ /* Title bar */}
-						<div className={styles.titleBar}>Send Cash</div>
+            {/* Manage daemon */}
+            <div className={styles.manageDaemonContainer}>
+              <div className={styles.manageDaemonTitle}>MANAGE DAEMON</div>
+              <button
+                className={styles.stopLocalNodeButton}
+                onClick={event => this.onStopLocalNodeClicked(event)}
+                onKeyDown={event => this.onStopLocalNodeClicked(event)}
+              >
+                {this.props.systemInfo.daemonInfo.status === 'RUNNING'
+                  ? 'STOP LOCAL NODE'
+                  : 'START LOCAL NODE'}
+              </button>
 
-						{ /* Old password */}
-						<RounedInput
-							name='old-password'
-							title='OLD PASSWORD'
-							addon={passwordAddon}
-							onInputChange={(value) => this.onOldPasswordInputChanged(value)}
-						/>
+              <button
+                className={styles.showStatusButton}
+                onClick={event => this.onShowStatusClicked(event)}
+                onKeyDown={event => this.onShowStatusClicked(event)}
+              >
+                SHOW STATUS
+              </button>
+            </div>
 
-						{ /* New password */}
-						<RounedInput
-							name='new-password'
-							title='NEW PASSWORD'
-							addon={passwordAddon}
-							onInputChange={(value) => this.onNewPasswordInputChanged(value)}
-						/>
+            {/* Manage wallet */}
+            <div className={styles.manageWalletContainer}>
+              <div className={styles.manageWalletTitle}>MANAGE WALLET</div>
 
-						{ /* Repeat password */}
-						<RounedInput
-							name='repeat-password'
-							title='REPEAT NEW PASSWORD'
-							addon={passwordAddon}
-							onInputChange={(value) => this.onRepeatPasswordInputChanged(value)}
-						/>
-
-						{ /* Save password */}
-						<button
-							className={styles.savePasswordButton}
-							onClick={(event) => this.onSavePasswordClicked(event)}
-							onKeyDown={(event) => this.onSavePasswordClicked(event)}
-						>
-							SAVE PASSWORD
-						</button>
-
-						{ /* Manage daemon */}
-						<div className={styles.manageDaemonContainer} >
-							<div className={styles.manageDaemonTitle}>MANAGE DAEMON</div>
-
-							<button
-								className={styles.stopLocalNodeButton}
-								onClick={(event) => this.onStopLocalNodeClicked(event)}
-								onKeyDown={(event) => this.onStopLocalNodeClicked(event)}
-							>
-								STOP LOCAL NODE
-							</button>
-
-							<button
-								className={styles.showStatusButton}
-								onClick={(event) => this.onShowStatusClicked(event)}
-								onKeyDown={(event) => this.onShowStatusClicked(event)}
-							>
-								SHOW STATUS
-							</button>
-						</div>
-
-						{ /* Manage wallet */}
-						<div className={styles.manageWalletContainer} >
-							<div className={styles.manageWalletTitle}>MANAGE WALLET</div>
-
-							<button
-								className={styles.backupWalletNodeButton}
-								onClick={(event) => this.onBackupWalletClicked(event)}
-								onKeyDown={(event) => this.onBackupWalletClicked(event)}
-							>
-								BACKUP WALLET
-							</button>
-						</div>
-
-					</div>
-
-				</div>
-
-			</div>
-		)
-	}
+              <button
+                className={styles.backupWalletNodeButton}
+                onClick={event => this.onBackupWalletClicked(event)}
+                onKeyDown={event => this.onBackupWalletClicked(event)}
+              >
+                BACKUP WALLET
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-const mapStateToProps = (state) => ({
-	sendCash: state.sendCash
-})
+const mapStateToProps = state => ({
+  systemInfo: state.systemInfo,
+  sendCash: state.sendCash
+});
 
 export default connect(mapStateToProps, null)(Settings);
