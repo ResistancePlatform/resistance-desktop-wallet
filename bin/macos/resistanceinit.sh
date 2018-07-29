02c1a02ef318d9ec35b32e186b600b9bc08e542c
Konstantin Alexandrov <konstantin@resistance.io>
2018-07-29 17:47:09 +0200

    [release-preparation] Binaries updated

---
+++ bin/macos/resistanceinit.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7c86526a82387a53423ba4ae0948a1a894e201555be664d94874f2f7158dc743
+size 2893
---
+++ bin/windows/init-configure.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:850d40607070de01090c8d540a0b31909a1304846cc70c5647252e56ba9f6027
+size 2873
