a300bf793ea6270fd9b37221d8752e8e21d376c2
Konstantin Alexandrov <konstantin@resistance.io>
2018-08-08 16:31:42 +0200

    [show-status]: Tabs added

--- app/components/settings/status-modal.js
+++ app/components/settings/status-modal.js
@@ -2,9 +2,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Modal from 'react-modal'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 import { appStore } from '../../state/store/configureStore'
 import { SettingsActions, SettingsState } from '../../state/reducers/settings/settings.reducer'
+import VLayout from '../../theme/v-box-layout.scss'
 import styles from './status-modal.scss'
 
 type Props = {
@@ -34,18 +36,45 @@ class StatusModal extends Component<Props> {
   render() {
     return (
       <Modal
-      isOpen={this.props.settings.isStatusModalOpen}
-      className={styles.statusModal}
-      contentLabel="Services Status"
+        isOpen={this.props.settings.isStatusModalOpen}
+        className={[styles.statusModal, VLayout.vBoxContainer].join(' ')}
+        contentLabel="Services Status"
       >
-      <h2>Hello</h2>
-      <button
-      onClick={event => this.onCloseStatusModalClicked(event)}
-      onKeyDown={event => this.onCloseStatusModalClicked(event)}
-      >
-      Close
-      </button>
-      <div>I am a modal</div>
+        <div className={[styles.modalTitle, VLayout.vBoxChild].join(' ')}>
+          <div
+            className={styles.closeButton}
+            onClick={event => this.onCloseStatusModalClicked(event)}
+            onKeyDown={event => this.onCloseStatusModalClicked(event)}
+          />
+          Services Status
+        </div>
+
+        <Tabs>
+          <TabList>
+            <Tab>Local Node</Tab>
+            <Tab>Miner</Tab>
+            <Tab>Tor</Tab>
+          </TabList>
+
+          <TabPanel>
+            Local Node Status
+          </TabPanel>
+
+          <TabPanel>
+            Miner Status
+          </TabPanel>
+
+          <TabPanel>
+            Tor Status
+          </TabPanel>
+        </Tabs>
+
+        <button
+          onClick={event => this.onCloseStatusModalClicked(event)}
+          onKeyDown={event => this.onCloseStatusModalClicked(event)}
+        >
+          Close
+        </button>
 
       </Modal>
     )
