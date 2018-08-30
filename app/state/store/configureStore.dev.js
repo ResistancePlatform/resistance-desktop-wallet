a38247e47c9768bccb35a65184aab092b7b59def
Konstantin Alexandrov <konstantin@resistance.io>
2018-08-30 12:13:34 +0200

    [114-refactoring]: Root reducer and root epic created

--- app/state/store/configureStore.dev.js
+++ app/state/store/configureStore.dev.js
@@ -1,55 +1,16 @@
-import { createStore, applyMiddleware, combineReducers, Store } from 'redux'
-import { combineEpics, createEpicMiddleware } from 'redux-observable'
+import { createStore, applyMiddleware, Store } from 'redux'
+import { createEpicMiddleware } from 'redux-observable'
 import { createHashHistory } from 'history'
 import { routerMiddleware } from 'react-router-redux'
 import { createLogger } from 'redux-logger'
-import {reducer as toastrReducer} from 'react-redux-toastr'
 
-import { RpcPollingReducer } from '../reducers/rpc-polling/rpc-polling.reducer'
-import { PopupMenuReducer } from '../reducers/popup-menu/popup-menu.reducer'
-import { NaviReducer } from '../reducers/navi/navi.reducer'
-import { NaviEpics } from '../reducers/navi/navi.epic'
-import { SystemInfoReducer } from '../reducers/system-info/system-info.reducer'
-import { SystemInfoEpics } from '../reducers/system-info/system-info.epic'
-import { OverviewReducer } from '../reducers/overview/overview.reducer'
-import { OverviewEpics } from '../reducers/overview/overview.epic'
-import { OwnAddressesReducer } from '../reducers/own-addresses/own-addresses.reducer'
-import { OwnAddressesEpics } from '../reducers/own-addresses/own-addresses.epic'
-import { SendCashReducer } from '../reducers/send-cash/send-cash.reducer'
-import { SendCashEpics } from '../reducers/send-cash/send-cash.epic'
-import { SettingsReducer } from '../reducers/settings/settings.reducer'
-import { SettingsEpics } from '../reducers/settings/settings.epic'
-import { AddressBookReducer } from '../reducers/address-book/address-book.reducer'
-import { AddressBookEpics } from '../reducers/address-book/address-book.epic'
+import { rootReducer, rootEpic } from '../reducers'
+
 
 export const history = createHashHistory()
-
-const appReducers = combineReducers({
-  toastr: toastrReducer,
-  rpcPolling: RpcPollingReducer,
-  popupMenu: PopupMenuReducer,
-	navi: NaviReducer,
-	systemInfo: SystemInfoReducer,
-	overview: OverviewReducer,
-	ownAddresses: OwnAddressesReducer,
-	sendCash: SendCashReducer,
-	settings: SettingsReducer,
-	addressBook: AddressBookReducer
-})
-
-export const appEpics = combineEpics(
-	NaviEpics,
-	SystemInfoEpics,
-	OverviewEpics,
-	OwnAddressesEpics,
-	SendCashEpics,
-	SettingsEpics,
-	AddressBookEpics
-)
+export let appStore: Store = null
 
 const epicMiddleware = createEpicMiddleware()
-
-export let appStore: Store = null
 
 export const configureStore = initialState => {
 	// Redux Configuration
@@ -75,12 +36,12 @@ export const configureStore = initialState => {
 
 	// Create Store
 	appStore = createStore(
-		appReducers,
+		rootReducer,
 		initialState,
 		applyMiddleware(...middleware)
 	)
 
-	epicMiddleware.run(appEpics)
+	epicMiddleware.run(rootEpic)
 
 	return appStore
 }
