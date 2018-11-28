8bfa06f62ca912190aba7da273800dd5d2606d17
Konstantin Alexandrov <konstantin@resistance.io>
2018-11-28 16:16:49 +0100

    Application version changed

--- app/components/resdex/Logo.js
+++ app/components/resdex/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import cn from 'classnames'
 
+import resDexLogo from '~/assets/images/resdex/logo.svg'
 import styles from './Logo.scss'
 
 const Logo = () => (
   <div className={cn(styles.container)}>
-    <img src="assets/images/resdex/logo.svg" alt="ResDEX" />
+    <img src={resDexLogo} alt="ResDEX" />
     ResDEX
   </div>
 )
